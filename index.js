da486abc9b04de6560628f3e4377ec3bd7a4548a
Arthur Schmitt <tart2000design@gmail.com>
2016-10-21 19:58:26 -0400

    Working prototype

---
+++ index.js
@@ -0,0 +1,253 @@
+if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+
+var container;
+var camera, controls, scene, renderer;
+var plane, cube;
+var mouse, raycaster, isShiftDown = false;
+
+var rollOverMesh, rollOverMaterial;
+var cubeGeo, cubeMaterial;
+
+var objects = [];
+
+init();
+animate();
+
+function init() {
+
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
+
+	var info = document.createElement( 'div' );
+	info.style.position = 'absolute';
+	info.style.top = '10px';
+	info.style.width = '100%';
+	info.style.textAlign = 'center';
+	info.innerHTML = 'Voxel builder<br><strong>click</strong>: add voxel, <strong>shift + click</strong>: remove voxel <a href="javascript:download();">Download STL</a>';
+	container.appendChild( info );
+
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
+	camera.position.set( 500, 800, 1300 );
+	camera.lookAt( new THREE.Vector3() );
+
+	controls = new THREE.OrbitControls( camera );
+	controls.addEventListener( 'change', render );
+
+	scene = new THREE.Scene();
+
+	// roll-over helpers
+
+	rollOverGeo = new THREE.BoxGeometry( 50, 50, 50 );
+	rollOverMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, opacity: 0.5, transparent: true } );
+	rollOverMesh = new THREE.Mesh( rollOverGeo, rollOverMaterial );
+	scene.add( rollOverMesh );
+
+	// cubes
+
+	cubeGeo = new THREE.BoxGeometry( 50, 50, 50 );
+	cubeMaterial = new THREE.MeshLambertMaterial( { color: 0xfeb74c} );
+
+	// grid
+
+	var size = 500, step = 50;
+
+	var geometry = new THREE.Geometry();
+
+	for ( var i = - size; i <= size; i += step ) {
+
+		geometry.vertices.push( new THREE.Vector3( - size, 0, i ) );
+		geometry.vertices.push( new THREE.Vector3(   size, 0, i ) );
+
+		geometry.vertices.push( new THREE.Vector3( i, 0, - size ) );
+		geometry.vertices.push( new THREE.Vector3( i, 0,   size ) );
+
+	}
+
+	var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2, transparent: true } );
+
+	var line = new THREE.LineSegments( geometry, material );
+	scene.add( line );
+	line.name = 'thelines';
+
+
+	raycaster = new THREE.Raycaster();
+	onMouseDownPosition = new THREE.Vector2();
+	onMouseUpPosition = new THREE.Vector2();
+	onGhostMove = new THREE.Vector2();
+
+	var geometry = new THREE.PlaneBufferGeometry( 1000, 1000 );
+	geometry.rotateX( - Math.PI / 2 );
+
+	plane = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { visible: false } ) );
+	scene.add( plane );
+	plane.name = 'theplane';
+
+	objects.push( plane );
+
+	// Lights
+
+	var ambientLight = new THREE.AmbientLight( 0x606060 );
+	scene.add( ambientLight );
+
+	var directionalLight = new THREE.DirectionalLight( 0xffffff );
+	directionalLight.position.set( 1, 0.75, 0.5 ).normalize();
+	scene.add( directionalLight );
+
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setClearColor( 0xf0f0f0 );
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	container.appendChild( renderer.domElement );
+	
+	document.addEventListener( 'keydown', onDocumentKeyDown, false );
+	document.addEventListener( 'keyup', onDocumentKeyUp, false );
+
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+	document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+
+
+	//
+	window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onDocumentMouseDown( event ) {
+	event.preventDefault();
+	onMouseDownPosition.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
+
+
+	isMouseDown = true;
+
+	render();
+
+}
+
+function onDocumentMouseMove( event ) {
+
+	event.preventDefault();
+
+	onGhostMove.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
+	raycaster.setFromCamera( onGhostMove, camera );
+
+	var intersects = raycaster.intersectObjects( objects );
+
+	if ( intersects.length > 0 ) {
+
+		var intersect = intersects[ 0 ];
+
+		rollOverMesh.position.copy( intersect.point ).add( intersect.face.normal );
+		rollOverMesh.position.divideScalar( 50 ).floor().multiplyScalar( 50 ).addScalar( 25 );
+
+	}
+
+	render();
+
+}
+
+function onDocumentMouseUp (event) {
+
+	event.preventDefault();
+
+	isMouseDown = false;
+
+	onMouseUpPosition.set( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1 );
+
+	if( onMouseDownPosition.distanceTo( onMouseUpPosition ) > 0.00001 ){ return; }
+
+	raycaster.setFromCamera( onMouseUpPosition, camera );
+
+	var intersects = raycaster.intersectObjects( objects );
+
+	if ( intersects.length > 0 ) {
+
+		var intersect = intersects[ 0 ];
+
+		// delete cube
+
+		if ( isShiftDown ) {
+
+			if ( intersect.object != plane ) {
+
+				scene.remove( intersect.object );
+
+				objects.splice( objects.indexOf( intersect.object ), 1 );
+
+			}
+
+		// create cube
+
+		} else {
+
+			var voxel = new THREE.Mesh( cubeGeo, cubeMaterial );
+			voxel.position.copy( intersect.point ).add( intersect.face.normal );
+			voxel.position.divideScalar( 50 ).floor().multiplyScalar( 50 ).addScalar( 25 );
+			scene.add( voxel );
+
+			objects.push( voxel );
+
+		}
+
+		render();
+
+	}
+}
+
+function onDocumentKeyDown( event ) {
+
+	switch( event.keyCode ) {
+
+		case 16: isShiftDown = true; break;
+
+	}
+
+}
+
+function onDocumentKeyUp( event ) {
+
+	switch ( event.keyCode ) {
+
+		case 16: isShiftDown = false; break;
+
+	}
+
+}
+
+function animate() {
+
+  requestAnimationFrame( animate );
+  controls.update();
+
+}
+
+function render() {
+
+	renderer.render( scene, camera );
+
+}
+
+
+function download() {
+
+	scene.remove( rollOverMesh );
+	scene.rotation.x = THREE.Math.degToRad(90);
+	scene.scale.set(0.2,0.2,0.2);
+	scene.updateMatrixWorld(); 
+
+	saveSTL(scene, 'model');
+
+	scene.scale.set(1,1,1);
+	scene.rotation.x = 0;
+	scene.add( rollOverMesh );
+}
+
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+# voxel-builder
+
+originally inspired by [voxel painter](http://mrdoob.com/projects/voxels/) by [mr doob](http://github.com/mrdoob).
+
+## Roadmap 
+*Any contributions welcome*
+
+## license
+
+BSD
